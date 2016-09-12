9ae7e0866e47200152b1305b1be0ddddd5dbd079
Tom Grace <tom@penguinanddaemoncom>
2016-09-12 15:29:17 +0100

    Extra coverage for top half of file

--- aptly/aptly_cli_test.go
+++ aptly/aptly_cli_test.go
@@ -17,6 +17,162 @@ type AptlyCliSuite struct{}
 
 var _ = Suite(&AptlyCliSuite{})
 
+func (s *AptlyCliSuite) TestGpgAddSuccess(c *C) {
+	a := AptlyCli{}
+    testGpgKey := "9E3E53F19C7DE460"
+    //Fake exec
+    execExec = fakeExecExec
+    defer func(){ execExec = exec.Exec }()
+    outstring, err := a.Gpg_add(testGpgKey)
+	c.Check(outstring[0], Equals, "gpg: requesting key 9C7DE460 from hkp server keys.gnupg.net")
+    c.Check(err, Equals, nil)
+}
+
+func (s *AptlyCliSuite) TestGpgAddFailure(c *C) {
+	a := AptlyCli{}
+    testGpgKey := "FAKE"
+    //Fake exec
+    execExec = fakeExecExec
+    defer func(){ execExec = exec.Exec }()
+    outstring, err := a.Gpg_add(testGpgKey)
+	c.Check(outstring[0], Equals, "gpg: requesting key FAKE from hkp server keys.gnupg.net")
+    c.Check(err, ErrorMatches, "exit status 2")
+}
+
+func (s *AptlyCliSuite) TestMirrorListSuccess(c *C) {
+	a := AptlyCli{}
+    //Fake exec
+    execExec = fakeExecExec
+    defer func(){ execExec = exec.Exec }()
+    outstring, err := a.Mirror_list()
+	c.Check(outstring[0], Equals, "some_mirror")
+    c.Check(err, Equals, nil)
+}
+
+func (s *AptlyCliSuite) TestMirrorUpdateSuccess(c *C) {
+	a := AptlyCli{}
+    //Fake exec
+    execExec = fakeExecExec
+    defer func(){ execExec = exec.Exec }()
+    testMirror := "working_mirror"
+    outstring, err := a.Mirror_update(testMirror)
+	c.Check(outstring[0], Equals, "Mirror `working_mirror` has been successfully updated.")
+    c.Check(err, Equals, nil)
+}
+
+func (s *AptlyCliSuite) TestMirrorUpdateFailure(c *C) {
+	a := AptlyCli{}
+    //Fake exec
+    execExec = fakeExecExec
+    defer func(){ execExec = exec.Exec }()
+    testMirror := "test_mirror_no_exist"
+    outstring, err := a.Mirror_update(testMirror)
+	c.Check(outstring[0], Equals, "ERROR: unable to update: mirror with name test_mirror_no_exist not found")
+    c.Check(err, ErrorMatches, "exit status 1")
+}
+
+func (s *AptlyCliSuite) TestMirrorCreateNofilter(c *C) {
+	a := AptlyCli{}
+    testData := mirror.AptlyMirrorStruct{}
+    testData.Name = "test_mirror"
+    testData.Url = "http://example.com"
+    testData.Dist = "test_dist"
+    testData.Component = "test_component"
+    testData.FilterDeps = true
+    //Fake exec
+    execExec = fakeExecExec
+    defer func(){ execExec = exec.Exec }()
+    outstring, err := a.Mirror_create(testData)
+	c.Check(outstring[4], Equals, "Mirror [test_mirror]: http://example.com test_dist successfully added.")
+    c.Check(err, Equals, nil)
+}
+func (s *AptlyCliSuite) TestMirrorCreateSinglefilter(c *C) {
+	a := AptlyCli{}
+    testData := mirror.AptlyMirrorStruct{}
+    var testFilters = []mirror.AptlyFilterStruct {
+        mirror.AptlyFilterStruct {
+            Name: "software1",
+            Version: "1.2.3",
+        },
+    }
+    testData.Name = "test_mirror"
+    testData.Url = "http://example.com"
+    testData.Dist = "test_dist"
+    testData.Filter = testFilters
+    testData.FilterDeps = true
+    //Fake exec
+    execExec = fakeExecExec
+    defer func(){ execExec = exec.Exec }()
+    outstring, err := a.Mirror_create(testData)
+	c.Check(outstring[4], Equals, "Mirror [test_mirror]: http://example.com test_dist successfully added.")
+    c.Check(err, Equals, nil)
+}
+func (s *AptlyCliSuite) TestMirrorCreateMultifilter(c *C) {
+	a := AptlyCli{}
+    testData := mirror.AptlyMirrorStruct{}
+    var testFilters = []mirror.AptlyFilterStruct {
+        mirror.AptlyFilterStruct {
+            Name: "software1",
+            Version: "1.2.3",
+        },
+        {
+            Name: "software2",
+            Version: "1.2.3",
+        },
+    }
+    testData.Name = "test_mirror"
+    testData.Url = "http://example.com"
+    testData.Dist = "test_dist"
+    testData.Component = "test_component"
+    testData.Filter = testFilters
+    testData.FilterDeps = true
+    //Fake exec
+    execExec = fakeExecExec
+    defer func(){ execExec = exec.Exec }()
+    outstring, err := a.Mirror_create(testData)
+	c.Check(outstring[4], Equals, "Mirror [test_mirror]: http://example.com test_dist successfully added.")
+    c.Check(err, Equals, nil)
+}
+
+func (s *AptlyCliSuite) TestMirrorCreateMissingName(c *C) {
+	a := AptlyCli{}
+    testData := mirror.AptlyMirrorStruct{}
+    testData.Url = "http://example.com"
+    testData.Dist = "test_dist"
+    testData.Component = "test_component"
+    testData.FilterDeps = true
+    //Fake exec
+    execExec = fakeExecExec
+    defer func(){ execExec = exec.Exec }()
+    _, err := a.Mirror_create(testData)
+    c.Check(err, ErrorMatches, "Missing name from mirror")
+}
+func (s *AptlyCliSuite) TestMirrorCreateMissingUrl(c *C) {
+	a := AptlyCli{}
+    testData := mirror.AptlyMirrorStruct{}
+    testData.Name = "test_name"
+    testData.Dist = "test_dist"
+    testData.Component = "test_component"
+    testData.FilterDeps = true
+    //Fake exec
+    execExec = fakeExecExec
+    defer func(){ execExec = exec.Exec }()
+    _, err := a.Mirror_create(testData)
+    c.Check(err, ErrorMatches, "Missing url from mirror")
+}
+func (s *AptlyCliSuite) TestMirrorCreateMissingDist(c *C) {
+	a := AptlyCli{}
+    testData := mirror.AptlyMirrorStruct{}
+    testData.Name = "test_name"
+    testData.Url = "http://example.com"
+    testData.Component = "test_component"
+    testData.FilterDeps = true
+    //Fake exec
+    execExec = fakeExecExec
+    defer func(){ execExec = exec.Exec }()
+    _, err := a.Mirror_create(testData)
+    c.Check(err, ErrorMatches, "Missing distribution from mirror")
+}
 func (s *AptlyCliSuite) TestCreateAptlyMirrorFilterCommand(c *C) {
 
 	testFilter1 := mirror.AptlyFilterStruct{}
@@ -174,11 +330,35 @@ Snapshot test_mirror_1970-01-01_00:00:00 successfully created.
 You can run 'aptly publish snapshot test_mirror_1970-01-01_00:00:00' to publish snapshot as Debian repository.`,
         "aptly snapshot create test_mirror_fail_1970-01-01_00:00:00 from mirror test_mirror_fail": `ERROR: unable to create snapshot: mirror with name test_mirror_fail not found`,
         "aptly mirror update test_mirror_no_exist": `ERROR: unable to update: mirror with name test_mirror_no_exist not found`,
+        "gpg --no-default-keyring --keyring trustedkeys.gpg --keyserver keys.gnupg.net --recv-keys 9E3E53F19C7DE460": `gpg: requesting key 9C7DE460 from hkp server keys.gnupg.net`,
+        "gpg --no-default-keyring --keyring trustedkeys.gpg --keyserver keys.gnupg.net --recv-keys FAKE": `gpg: requesting key FAKE from hkp server keys.gnupg.net`,
+        "aptly mirror list -raw": `some_mirror`,
+        "aptly mirror update broken_mirror": `ERROR: unable to update: mirror with name broken_mirror not found`,
+        "aptly mirror update working_mirror": "Mirror `working_mirror` has been successfully updated.",
+        "aptly mirror create -filter=( Name (= software1 ) , $Version (= 1.2.3 ) ) | ( Name (= software2 ) , $Version (= 1.2.3 ) ) -filter-with-deps test_mirror http://example.com test_dist test_component": `Downloading http://ppa.launchpad.net/vbernat/haproxy-1.5/ubuntu/dists/trusty/InRelease...
+gpgv: Signature made Fri May 13 12:29:45 2016 UTC using RSA key ID 1C61B9CD
+gpgv: Good signature from "Launchpad PPA for Vincent Bernat"
+
+Mirror [test_mirror]: http://example.com test_dist successfully added.
+You can run 'aptly mirror update test_mirror' to download repository contents.`,
+        "aptly mirror create -filter=( Name (= software1 ) , $Version (= 1.2.3 ) ) -filter-with-deps test_mirror http://example.com test_dist":  `Downloading http://ppa.launchpad.net/vbernat/haproxy-1.5/ubuntu/dists/trusty/InRelease...
+gpgv: Signature made Fri May 13 12:29:45 2016 UTC using RSA key ID 1C61B9CD
+gpgv: Good signature from "Launchpad PPA for Vincent Bernat"
+
+Mirror [test_mirror]: http://example.com test_dist successfully added.
+You can run 'aptly mirror update test_mirror' to download repository contents.`,
+        "aptly mirror create -filter-with-deps test_mirror http://example.com test_dist test_component":  `Downloading http://ppa.launchpad.net/vbernat/haproxy-1.5/ubuntu/dists/trusty/InRelease...
+gpgv: Signature made Fri May 13 12:29:45 2016 UTC using RSA key ID 1C61B9CD
+gpgv: Good signature from "Launchpad PPA for Vincent Bernat"
+
+Mirror [test_mirror]: http://example.com test_dist successfully added.
+You can run 'aptly mirror update test_mirror' to download repository contents.`,
     }
     testError:= map[string]int{
         "aptly mirror update test_mirror_no_exist": 1,
         "aptly snapshot merge testCombinedSnapshot input1 input_no_exist": 1,
         "aptly snapshot create test_mirror_fail_1970-01-01_00:00:00 from mirror test_mirror_fail": 1,
+        "gpg --no-default-keyring --keyring trustedkeys.gpg --keyserver keys.gnupg.net --recv-keys FAKE": 2,
     }
 	if len(os.Args) > 2 {
 		if os.Args[1] == "-test.run=TestHelperProcess" {
